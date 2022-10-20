05b46d892313857e09376532e9ac4e2aba64866e
willaCS <arthur.willems.prive@gmail.com>
2022-10-20 10:05:44 +0200

    reset for new branch

--- src/utils/cube3d_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef CUBE3D_UTILS_H
-#define CUBE3D_UTILS_H
-
-void	*struct_set(void *var, int size);
-
-#endif /* CUBE3D_UTILS_H */
--- src/utils/debug/cube3d_debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef CUBE3D_DEBUG_H
-#define CUBE3D_DEBUG_H
-
-int	ret_print(int ret, const char *msg);
-
-# define ERR_FILE_EXT  "Error: bad file extension !\n"
-
-#endif /* CUBE3D_DEBUG_H */
