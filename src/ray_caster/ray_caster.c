c931e026394b1086a2cfa8970a023b5aac84305a
willaCS <arthur.willems.prive@gmail.com>
2022-11-04 10:25:15 +0100

    ++

--- src/ray_caster/ray_caster.c
+++ src/ray_caster/ray_caster.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 10:01:39 by awillems          #+#    #+#             */
-/*   Updated: 2022/11/02 14:47:29 by awillems         ###   ########.fr       */
+/*   Updated: 2022/11/04 10:24:21 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ void ray_caster(t_game *game)
 		t_intersect inter = get_intersect(game, game->player.coord,
 			loop_len(game->player.alpha - FOV_ANGLE1_2 + FOV_INCRE * i, PI2));
 		double dist = distance(game, inter);
-		// if (dist < MAX_DIST)
-		// 	dist = MAX_DIST;
+		if (dist < MAX_DIST)
+			dist = MAX_DIST;
 		draw_simple(game, inter, i * COLUMN_WIDTH, HEIGTH_OF_BLOCK / dist);
 		draw_column(game, inter, i * COLUMN_WIDTH, HEIGTH_OF_BLOCK / dist);
 	}
