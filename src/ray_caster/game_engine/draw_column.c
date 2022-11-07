b49685ac8286e7b9aaa9432957311c1946cad59b
willaCS <arthur.willems.prive@gmail.com>
2022-11-07 14:05:50 +0100

    wip

--- src/ray_caster/game_engine/draw_column.c
+++ src/ray_caster/game_engine/draw_column.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 10:31:46 by awillems          #+#    #+#             */
-/*   Updated: 2022/11/07 13:14:39 by awillems         ###   ########.fr       */
+/*   Updated: 2022/11/07 14:04:04 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,13 @@
 
 int is_equal(double a, double b);
 
-uint32_t get_color_for_direction(const t_inter inter)
-{
-	if (inter.wall.x - 0.00001 < inter.point.x && inter.point.x < inter.wall.x + 0.00001)
-		return (0xeb4034FF);
-	if (inter.wall.x + 0.99999 < inter.point.x && inter.point.x < inter.wall.x + 1.00001)
-		return (0x4287f5FF);
-	if (inter.wall.y - 0.00001 < inter.point.y && inter.point.y < inter.wall.y + 0.00001)
-		return (0xfcba03FF);
-	if (inter.wall.y + 0.99999 < inter.point.y && inter.point.y < inter.wall.y + 1.00001)
-		return (0x32a852FF);
-	return (0);
-}
+t_chunk get_chunk(t_game *game, t_coord_i32 coord);
+t_texture *get_wall_texture(t_chunk *chunk, t_coord_f64 inter);
 
-void draw_simple(t_game *game, t_inter inter, uint32_t x, int32_t height)
+t_texture *get_texture(t_game *game, t_inter inter)
 {
-	draw_rectangle(&game->param,
-		set_f64(x, WIN_HEIGHT / 2 - height),
-		set_i32(COLUMN_WIDTH, height * 2), 
-		get_color_for_direction(inter)
-	);
-}
-
-
-t_texture get_texture(t_game *game, int x, int y)
-{
-	(void) x;
-	(void) y;
-	return (game->temp);
+	t_chunk chunk = get_chunk(game, inter.wall);
+	return (get_wall_texture(&chunk, inter.point));
 }
 
 t_coord_f64 get_texture_inter(t_inter inter)
@@ -68,32 +47,34 @@ t_coord_f64 get_texture_inter(t_inter inter)
  */
 void draw_column(t_game *game, t_inter inter, uint32_t x, int32_t height)
 {
-	const t_texture texture = get_texture(game, inter.wall.x, inter.wall.y);
+	const t_texture *texture = get_texture(game, inter);
 	const t_coord_f64 texture_inter = get_texture_inter(inter);
 	const int32_t	parse_heigth = (int32_t []){height, WIN_HEIGHT}[height > WIN_HEIGHT];
 	t_coord_f64 ratio;
 	double 		offset;
 	
-	if (texture.type == IMAGE && texture.image)
+	if (!texture)
+		return ;
+	if (texture->type == IMAGE && texture->image)
 	{
 		ratio = set_f64(1,
-			(double) texture.image->height /( (double) height * 2)
+			(double) texture->image->height /( (double) height * 2)
 		);
-		offset = texture_inter.x * texture.image->width
-				+ texture_inter.y * texture.image->height;
+		offset = texture_inter.x * texture->image->width
+				+ texture_inter.y * texture->image->height;
 	}
 	
 	for (int i = 0; i < COLUMN_WIDTH; i++)
 	{
 		for (int j = 0; j < parse_heigth * 2; j++)
 		{
-			if (texture.type == COLOR)
+			if (texture->type == COLOR)
 				put_pixel(&game->param, x + i, WIN_HEIGHT / 2 - height + j,
-					texture.color
+					texture->color
 				);
 			else
 				put_pixel(&game->param, x + i, WIN_HEIGHT / 2 - height + j,
-					get_pixel_image(texture, i + offset, j, ratio)
+					get_pixel_image(*texture, i + offset, j, ratio)
 				);
 		}
 		
