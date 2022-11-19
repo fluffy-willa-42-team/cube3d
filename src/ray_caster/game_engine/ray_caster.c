47b4cda26bd93db4a4770c4572ba7a8bac7c3de0
willaCS <arthur.willems.prive@gmail.com>
2022-11-19 12:57:46 +0100

    cleaner ray_caster

--- src/ray_caster/game_engine/ray_caster.c
+++ src/ray_caster/game_engine/ray_caster.c
@@ -6,33 +6,22 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 10:01:39 by awillems          #+#    #+#             */
-/*   Updated: 2022/11/19 12:50:10 by awillems         ###   ########.fr       */
+/*   Updated: 2022/11/19 12:57:26 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray_caster.h"
 #include "mlx_utils.h"
-#include <math.h>
+
 #include <stdio.h>
 
-double loop_len(double n, double len);
-t_inter get_intersect(t_game *game, t_coord_f64 player, double alpha, double tan_a);
+double	prot_tan(double alpha);
+double	loop_len(double n, double len);
+t_inter	get_intersect(t_game *game, t_coord_f64 player, double alpha, double tan_a);
 
 void	draw_skybox(t_game *game);
 void	draw_floor(t_game *game, int x, double alpha, double heigth_drawn, double dist);
 void	draw_wall(t_game *game, t_inter inter, uint32_t x, uint32_t height);
-
-double prot_tan(double alpha)
-{
-	double res;
-
-	res = tan(alpha);
-	if (res >= 0 && res < 0.0001)
-		res = 0.0001;
-	else if (res <= -0 && res > -0.0001)
-		res = -0.0001;
-	return (res);
-}
 
 static double distance(t_game *game, t_inter inter)
 {
@@ -40,6 +29,15 @@ static double distance(t_game *game, t_inter inter)
 		+ (inter.point.y - game->player.coord.y) * game->player.cosin.y);
 }
 
+/**
+ * @brief Will calculate the wall intersection draw the wall then the ground and
+ * ceiling then draw the last transparent wall.
+ * 
+ * @param game Pointer to the game instance.
+ * @param x The coordinate x of the column of the screen
+ * @param alpha The angle at which we are looking at that column
+ * @param tan_a The tangent of alpha
+ */
 void draw_column(t_game *game, uint32_t x, double alpha, double tan_a)
 {
 	t_inter	inter;
@@ -63,13 +61,15 @@ void draw_column(t_game *game, uint32_t x, double alpha, double tan_a)
 
  * @note Brief Explonation of the Ray Casting :
 
-Ray Casting can be subdivised in 3 step :
+Ray Casting can be subdivised in 4 step :
  1.	Dray a line at the middle of the screen which each pixel is an intersection
 	of the wall it finds. This step is more easy to do from a 2d perspective from
 	the top, which is like a minimap from a top bottom perspective.
  2.	For each pixel on the line we have drown extend it based on the distance of
 	the intersection of the wall. The further it is, the small the wall is.
- 3.	The last step is to fill in the rest which is the ceiling and the floor.
+ 3.	The third step is to fill in the rest which is the ceiling and the floor.
+ 4.	(Optional) The last step is to draw every transparent wall on top of what we
+	have drown.
 
 All of these steps are done on a column but column basis.
 
--- src/utils/math/math_utils.c
+++ src/utils/math/math_utils.c
@@ -6,11 +6,24 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 10:30:13 by awillems          #+#    #+#             */
-/*   Updated: 2022/11/07 11:24:02 by awillems         ###   ########.fr       */
+/*   Updated: 2022/11/19 12:51:47 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <math.h>
+
+double prot_tan(double alpha)
+{
+	double res;
+
+	res = tan(alpha);
+	if (res >= 0 && res < 0.0001)
+		res = 0.0001;
+	else if (res <= -0 && res > -0.0001)
+		res = -0.0001;
+	return (res);
+}
 
 /**
  * @brief Loops a number so it fits between 0 and the given len
