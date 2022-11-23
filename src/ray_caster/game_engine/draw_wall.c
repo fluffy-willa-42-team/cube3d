15b860a1a9626cfd1c2464aa5641bc5adf763448
willaCS <arthur.willems.prive@gmail.com>
2022-11-23 16:49:51 +0100

    ++

--- src/ray_caster/game_engine/draw_wall.c
+++ src/ray_caster/game_engine/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 14:46:26 by awillems          #+#    #+#             */
-/*   Updated: 2022/11/23 16:30:54 by awillems         ###   ########.fr       */
+/*   Updated: 2022/11/23 16:35:25 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,32 +34,31 @@ int is_transparent(t_texture *text)
 	return (!text || text->type & TRANSPARENCY);
 }
 
-void	draw_pixel_wall(t_game *game, t_coord_i32 pos)
+void	draw_pixel_wall(t_game *game, t_coord_i32 pos, t_texture *texture)
 {
-	put_pixel(game, pos.x, pos.y, 0xFF0000FF);
+	if (texture->type & IMAGE)
+		put_pixel(game, pos.x, pos.y, 0xFF0000FF);
+	else
+		put_pixel(game, pos.x, pos.y, texture->color);
 }
 
-void	draw_wall_text(t_game *game, t_texture *text, uint32_t x, double height)
+void	draw_wall_text(t_game *game, t_texture *texture, uint32_t x, double height)
 {
-	(void) text;
-	if (!text)
+	if (!texture)
 		return ;
 	uint32_t parse_heigth = height;
 	if (parse_heigth >= MIDDLE_OF_SCREEN)
 		parse_heigth = MIDDLE_OF_SCREEN;
 	for (uint32_t i = height - parse_heigth; i < height + parse_heigth ; i++)
-		draw_pixel_wall(game, set_i32(x, MIDDLE_OF_SCREEN - height + i));
+		draw_pixel_wall(game, set_i32(x, MIDDLE_OF_SCREEN - height + i), texture);
 }
 
-// cas 1 : le mur devant est n'est pas transparent
-// cas 2 : le mur devant est transparent ou nexiste pas et le mur derriere est
-// 		   solide
 void	draw_wall(t_game *game, uint32_t x, t_coord_f64 inter, double height)
 {
 	t_wall_inter wall = get_wall(game, inter);
 	
-	if ((is_equal(inter.y, (int) inter.y) && inter.y > game->player.pos.y)
-		|| (is_equal(inter.x, (int) inter.x) && inter.x > game->player.pos.x))
+	if ((is_equal(inter.y, (int) (float) inter.y) && inter.y > game->player.pos.y)
+		|| (is_equal(inter.x, (int) (float) inter.x) && inter.x > game->player.pos.x))
 		exchange_textures(&wall);
 	if (!is_transparent(wall.text1))
 		draw_wall_text(game, wall.text1, x, height);
