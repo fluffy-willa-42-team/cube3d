d776e24c0b9b54a5a7704f370ab69005a0d0a06e
willaCS <arthur.willems.prive@gmail.com>
2022-11-25 11:50:20 +0100

    better organisation

---
+++ src/hooks_loop.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks_loop.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 11:47:01 by awillems          #+#    #+#             */
+/*   Updated: 2022/11/25 11:48:49 by awillems         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+#include "ray_caster.h"
+#include "mlx_utils.h"
+
+void	move_player(t_game *game, t_coord_f64 player, t_coord_f64 incr);
+void	pos_hook(double incr_x, double incr_y, t_coord_f64 *move_vec);
+void	angle_hook(t_game *game, double incrementation);
+void	scale_hook(t_game *game, double incrementation);
+void	map_hook(t_game *game, int32_t incrementation);
+void	map_point_hook(t_game *game, double incrementation);
+
+void	hook_loop(void *param)
+{
+	t_game		*game = param;
+	t_coord_f64	*cosin = &game->player.cosin;
+	t_coord_f64	move_vec = set_f64(0, 0);
+
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(game->param.mlx);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_UP))
+		pos_hook(0.02 * cosin->x, 0.02 * cosin->y, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_DOWN))
+		pos_hook(-0.02 * cosin->x, -0.02 * cosin->y, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_LEFT))
+		pos_hook(0.02 * cosin->y, -0.02 * cosin->x, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_RIGHT))
+		pos_hook(-0.02 * cosin->y, 0.02 * cosin->x, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_5))
+		pos_hook(0.1 * cosin->x, 0.1 * cosin->y, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_2))
+		pos_hook(-0.1 * cosin->x, -0.1 * cosin->y, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_1))
+		pos_hook(0.1 * cosin->y, -0.1 * cosin->x, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_3))
+		pos_hook(-0.1 * cosin->y, 0.1 * cosin->x, &move_vec);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_Y))
+		scale_hook(game, 0.1);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_H))
+		scale_hook(game, -0.1);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_U))
+		scale_hook(game, 0.01);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_J))
+		scale_hook(game, -0.01);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_I))
+		map_point_hook(game, 0.1);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_K))
+		map_point_hook(game, -0.1);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_O))
+		map_hook(game, 1);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_L))
+		map_hook(game, -1);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_A))
+		angle_hook(game, -0.03);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_D))
+		angle_hook(game, 0.03);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_Q))
+		angle_hook(game, -0.001);
+	if (mlx_is_key_down(game->param.mlx, MLX_KEY_E))
+		angle_hook(game, 0.001);
+	if (move_vec.x != 0 || move_vec.y != 0)
+		move_player(game, game->player.pos, move_vec);
+	draw_rectangle(game, set_f64(0, 0), set_i32(WIN_WIDTH, WIN_HEIGHT), 0x000000ff);
+	ray_caster(game);
+	draw_minimap(game);
+}
--- src/main.c
+++ src/main.c
@@ -6,88 +6,24 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 10:49:27 by awillems          #+#    #+#             */
-/*   Updated: 2022/11/25 11:44:33 by awillems         ###   ########.fr       */
+/*   Updated: 2022/11/25 11:49:11 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include "MLX42/MLX42.h"
 
 #include "cube3d.h"
-#include "ray_caster.h"
-#include "MLX42/MLX42.h"
-#include "mlx_utils.h"
-
 #include "init_data.h"
 
-void	move_player(t_game *game, t_coord_f64 player, t_coord_f64 incr);
-void	pos_hook(double incr_x, double incr_y, t_coord_f64 *move_vec);
-void	angle_hook(t_game *game, double incrementation);
-void	scale_hook(t_game *game, double incrementation);
-void	map_hook(t_game *game, int32_t incrementation);
-void	map_point_hook(t_game *game, double incrementation);
-
-void	hook(void *param)
-{
-	t_game		*game = param;
-	t_coord_f64	*cosin = &game->player.cosin;
-	t_coord_f64	move_vec = set_f64(0, 0);
-
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(game->param.mlx);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_UP))
-		pos_hook(0.02 * cosin->x, 0.02 * cosin->y, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_DOWN))
-		pos_hook(-0.02 * cosin->x, -0.02 * cosin->y, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_LEFT))
-		pos_hook(0.02 * cosin->y, -0.02 * cosin->x, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_RIGHT))
-		pos_hook(-0.02 * cosin->y, 0.02 * cosin->x, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_5))
-		pos_hook(0.1 * cosin->x, 0.1 * cosin->y, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_2))
-		pos_hook(-0.1 * cosin->x, -0.1 * cosin->y, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_1))
-		pos_hook(0.1 * cosin->y, -0.1 * cosin->x, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_KP_3))
-		pos_hook(-0.1 * cosin->y, 0.1 * cosin->x, &move_vec);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_Y))
-		scale_hook(game, 0.1);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_H))
-		scale_hook(game, -0.1);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_U))
-		scale_hook(game, 0.01);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_J))
-		scale_hook(game, -0.01);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_I))
-		map_point_hook(game, 0.1);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_K))
-		map_point_hook(game, -0.1);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_O))
-		map_hook(game, 1);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_L))
-		map_hook(game, -1);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_A))
-		angle_hook(game, -0.03);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_D))
-		angle_hook(game, 0.03);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_Q))
-		angle_hook(game, -0.001);
-	if (mlx_is_key_down(game->param.mlx, MLX_KEY_E))
-		angle_hook(game, 0.001);
-	if (move_vec.x != 0 || move_vec.y != 0)
-		move_player(game, game->player.pos, move_vec);
-	draw_rectangle(game, set_f64(0, 0), set_i32(WIN_WIDTH, WIN_HEIGHT), 0x000000ff);
-	ray_caster(game);
-	draw_minimap(game);
-}
 
 t_texture *init_image(t_texture *ptr, char *path);
 t_texture *init_color(t_texture *ptr, uint32_t color);
-
 t_texture *init_texture(t_texture *ptr, int type, char *path, uint32_t color);
-void init_map(t_game *game);
+
+void	init_map(t_game *game);
+
+void	hook_loop(void *param);
 
 int main(void)
 {
@@ -170,7 +106,7 @@ int main(void)
 	if (!game.param.img)
 		return (EXIT_FAILURE);
 	mlx_image_to_window(game.param.mlx, game.param.img, 0, 0);
-	mlx_loop_hook(game.param.mlx, &hook, &game);
+	mlx_loop_hook(game.param.mlx, &hook_loop, &game);
 	mlx_loop(game.param.mlx);
 
 	if (game.temp.image)
