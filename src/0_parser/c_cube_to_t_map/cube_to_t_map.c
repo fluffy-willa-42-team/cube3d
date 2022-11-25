e62aeb6c37360cbb2800578c7818132e91c411e7
Matthew-Dreemurr <hadadmat@gmail.com>
2022-11-25 13:09:25 +0100

    [WIP] move debug

--- src/0_parser/c_cube_to_t_map/cube_to_t_map.c
+++ src/0_parser/c_cube_to_t_map/cube_to_t_map.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:12:15 by mahadad           #+#    #+#             */
-/*   Updated: 2022/11/25 12:53:52 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/11/25 13:04:19 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 
 /* EXIT_SUCCESS, EXIT_FAILURE*/
 #include <stdlib.h>
-
-#include <stdio.h>//TODO REMOVE
 
 #include "cube3d_utils.h"
 
@@ -40,90 +38,6 @@
  *           t_chunk_struct {TOP,BOTTOM,N,S,E,W,C,F,ENTITY,TAG,...}
  * 
  */
-
-
-//TODO REMOVE
-/******************************************************************************/
-/* */ // Get the red channel.
-/* */ int get_r(int rgba)
-/* */ {
-/* */     // Move 3 bytes to the right and mask out the first byte.
-/* */     return ((rgba >> 24) & 0xFF);
-/* */ }
-/* */ 
-/* */ // Get the green channel.
-/* */ int get_g(int rgba)
-/* */ {
-/* */     // Move 2 bytes to the right and mask out the first byte.
-/* */     return ((rgba >> 16) & 0xFF);
-/* */ }
-/* */ 
-/* */ // Get the blue channel.
-/* */ int get_b(int rgba)
-/* */ {
-/* */     // Move 1 byte to the right and mask out the first byte.
-/* */     return ((rgba >> 8) & 0xFF);
-/* */ }
-/* */ 
-/* */ // Get the alpha channel.
-/* */ int get_a(int rgba)
-/* */ {
-/* */     // Move 0 bytes to the right and mask out the first byte.
-/* */     return (rgba & 0xFF);
-/* */ }
-/******************************************************************************/
-
-int	tex_debug(t_parser *data)
-{
-	int tmp = 0;
-	for (int i = '!'; i < 125; i++)
-	{
-		tmp = get_tex_ptr(&data->tex_list, i)->type;
-		if (tmp > 0)
-			printf(
-			"[%c][%p] {\n"
-			"       type          :  {\n"
-			"                          UNDEFINED     [%d]\n"
-			"                          VALID         [%d]\n"
-			"                          COLOR         [%d]\n"
-			"                          IMAGE         [%d]\n"
-			"                          SKYBOX        [%d]\n"
-			"                          ALLOW_CLIP    [%d]\n"
-			"                          TRANSPARENCY  [%d]\n"
-			"                        },\n"
-			"       token         :  \'%c\',\n"
-			"       *path         :  \"%.15s\",\n"
-			"       *image        :  [%p],\n"
-			"       sky_box_token :  \'%c\',\n"
-			"       *skybox_tex   :  [%p],\n"
-			"       color         :  [%d, %d, %d, %d]\n"
-			"     }\n",
-			(char)i,
-			(get_tex_ptr(&data->tex_list, i)),
-
-			(get_tex_ptr(&data->tex_list, i)->type & UNDEFINED) != 0,
-			(get_tex_ptr(&data->tex_list, i)->type & VALID) != 0,
-			(get_tex_ptr(&data->tex_list, i)->type & COLOR) != 0,
-			(get_tex_ptr(&data->tex_list, i)->type & IMAGE) != 0,
-			(get_tex_ptr(&data->tex_list, i)->type & SKYBOX) != 0,
-			(get_tex_ptr(&data->tex_list, i)->type & ALLOW_CLIP) != 0,
-			(get_tex_ptr(&data->tex_list, i)->type & TRANSPARENCY) != 0,
-			
-			get_tex_ptr(&data->tex_list, i)->token,
-			get_tex_ptr(&data->tex_list, i)->path,
-			get_tex_ptr(&data->tex_list, i)->image,
-
-			
-			get_tex_ptr(&data->tex_list, i)->sky_box_token,
-			get_tex_ptr(&data->tex_list, i)->skybox_tex,
-
-			get_r(get_tex_ptr(&data->tex_list, i)->color),
-			get_g(get_tex_ptr(&data->tex_list, i)->color),
-			get_b(get_tex_ptr(&data->tex_list, i)->color),
-			get_a(get_tex_ptr(&data->tex_list, i)->color));
-	}
-	return (EXIT_SUCCESS);
-}
 
 /**
  * @brief Check if all character used in the map is authorize and if there
@@ -163,6 +77,5 @@ int	cube_to_t_map(t_parser *data)
 		data->cube_map++;
 	if (sani_map(data) || store_map(data))
 		return (EXIT_FAILURE);
-		/*TODO REMOVE*/tex_debug(data);/*TODO REMOVE*/
 	return (EXIT_SUCCESS);
 }
