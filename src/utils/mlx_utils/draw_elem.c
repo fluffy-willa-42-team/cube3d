10715260656463698b84458e7e1ee1507eae0c99
willaCS <arthur.willems.prive@gmail.com>
2022-12-15 12:07:48 +0100

    fix color

--- src/utils/mlx_utils/draw_elem.c
+++ src/utils/mlx_utils/draw_elem.c
@@ -12,13 +12,11 @@
 
 #include "cube3d.h"
 #include "math.h"
-#include <stdio.h>
 
 u_int32_t put_pixel_in_image(t_image *texture, uint32_t x, uint32_t y, uint32_t color);
 
 void	put_pixel(t_game *game, int32_t x, int32_t y, uint32_t color)
 {
-	// printf("%.8x\n", color);
 	if (color != 0x000000ff)
 		put_pixel_in_image(game->param.img, x, y, (color >> 8) + (color << 24));
 }
