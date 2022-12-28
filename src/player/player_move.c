7877117fa8ee686e7503be15fe0accc12c6b8041
willaCS <arthur.willems.prive@gmail.com>
2022-12-28 11:58:32 +0100

    a mess

--- src/player/player_move.c
+++ src/player/player_move.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 18:18:47 by awillems          #+#    #+#             */
-/*   Updated: 2022/12/28 11:48:50 by awillems         ###   ########.fr       */
+/*   Updated: 2022/12/28 11:55:17 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,22 +55,24 @@ void	player_move(t_game *game, t_coord_f64 player, t_coord_f64 incr)
 
 	double	xinter_p1_p2 = (int) player.x + (int []){0, 1}[incr.x > 0];
 	double	xinter_p2_r = (int) destination.x + (int []){0, 1}[incr.x > 0];
-	if (((int) player.x != (int)(player.x + incr.x)
-		&& is_a_wall_move(game, set_f64(xinter_p1_p2, player.y)))
-		|| (((int) player.x == (int)(player.x + incr.x))
-		&& (fabs(xinter_p2_r - destination.x) <= ray)
-		&& is_a_wall_move(game, set_f64(xinter_p2_r, destination.y))
-		))
+	if ((int) player.x != (int)(player.x + incr.x))
+	{
+		if (is_a_wall_move(game, set_f64(xinter_p1_p2, player.y)))
+			destination.x = xinter_p1_p2 + (int []){ray, -ray}[incr.x > 0];
+	}
+	else if (fabs(xinter_p2_r - destination.x) <= ray
+		&& is_a_wall_move(game, set_f64(xinter_p2_r, destination.y)))
 		destination.x = xinter_p2_r + (int []){ray, -ray}[incr.x > 0];
 		
 	double	yinter_p1_p2 = (int) player.y + (int []){0, 1}[incr.y > 0];
 	double	yinter_p2_r = (int) destination.y + (int []){0, 1}[incr.y > 0];
-	if (((int) player.y != (int)(player.y + incr.y)
-		&& is_a_wall_move(game, set_f64(player.x, yinter_p1_p2)))
-		|| (((int) player.y == (int)(player.y + incr.y))
-		&& (fabs(yinter_p2_r - destination.y) <= ray)
-		&& is_a_wall_move(game, set_f64(destination.x, yinter_p2_r))
-		))
-		destination.y = yinter_p1_p2 + (int []){ray, -ray}[incr.y > 0];
+	if ((int) player.y != (int)(player.y + incr.y))
+	{
+		if (is_a_wall_move(game, set_f64(player.x, yinter_p1_p2)))
+			destination.y = yinter_p1_p2 + (int []){ray, -ray}[incr.y > 0];
+	}
+	else if ((fabs(yinter_p2_r - destination.y) <= ray)
+		&& is_a_wall_move(game, set_f64(destination.x, yinter_p2_r)))
+		destination.y = yinter_p2_r + (int []){ray, -ray}[incr.y > 0];
 	game->player.pos = destination;
 }
