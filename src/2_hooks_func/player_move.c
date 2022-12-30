7a19cb651c03148320f54c7cfa1d81d10bbf34c2
willaCS <arthur.willems.prive@gmail.com>
2022-12-30 11:59:41 +0100

    ++

--- src/2_hooks_func/player_move.c
+++ src/2_hooks_func/player_move.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 18:18:47 by awillems          #+#    #+#             */
-/*   Updated: 2022/12/30 11:43:35 by awillems         ###   ########.fr       */
+/*   Updated: 2022/12/30 11:56:52 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	player_move(t_game *game, t_coord_f64 player, t_coord_f64 incr)
 	t_coord_f64			dest;
 	const t_coord_f64	x_step = set_f64(
 			(int) player.x + (int []){0, 1}[incr.x > 0],
-			(int)(player.x + incr.x) + (int []){0, 1}[incr.x > 0]);
+			(int)(player.x + incr.x) + (int []){0, 1}[incr.y > 0]);
 	t_coord_f64			y_step;
 
 	dest = set_f64(player.x + incr.x, player.y + incr.y);
@@ -55,7 +55,7 @@ void	player_move(t_game *game, t_coord_f64 player, t_coord_f64 incr)
 	else if (fabs(x_step.y - dest.x) <= ray
 		&& is_a_wall_move(game, set_f64(x_step.y, dest.y)))
 		dest.x = player.x;
-	y_step = set_f64((int) dest.y + (int []){0, 1}[incr.y > 0],
+	y_step = set_f64((int) dest.y + (int []){0, 1}[incr.x > 0],
 			(int)(dest.y + incr.y) + (int []){0, 1}[incr.y > 0]);
 	if ((int) player.y != (int)(dest.y)
 		&& is_a_wall_move(game, set_f64(dest.x, y_step.x)))
